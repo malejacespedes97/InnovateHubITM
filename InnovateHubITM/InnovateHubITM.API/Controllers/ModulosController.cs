e742f00fd8eed817180288512b10e99a755cbc3d
SARA MONCADA ARANGO <saraarango857@gmail.com>
2023-10-12 02:49:53 -0500

    Creación controladores Comentarios, Modulos y Profesores.

--- InnovateHubITM/InnovateHubITM.API/Controllers/ModulosController.cs
+++ InnovateHubITM/InnovateHubITM.API/Controllers/ModulosController.cs
@@ -7,25 +7,26 @@ namespace ProyectoInvestigacion.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProfesoresController : Controller
+    public class ModulosController : Controller
     {
         private readonly DataContext _context;
 
-        public ProfesoresController(DataContext context)
+        public ModulosController(DataContext context)
         {
+
             _context = context;
         }
 
         [HttpGet]//get por lista
         public async Task<ActionResult> Get()
         {
-            return Ok(await _context.Profesores.ToListAsync());
+            return Ok(await _context.Modulos.ToListAsync());
         }
 
         [HttpGet("{id}")]//get por parametro
         public async Task<ActionResult> Get(int id)
         {
-            var country = await _context.Profesores.FirstOrDefaultAsync(x => x.Id == id);
+            var country = await _context.Modulos.FirstOrDefaultAsync(x => x.Id == id);
             if (country == null)
             {
                 return NotFound();
@@ -34,12 +35,12 @@ namespace ProyectoInvestigacion.API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> Post(Profesor profesor) //siempre los post son iguales, solo cambia el nombre de la entidad
+        public async Task<ActionResult> Post(Modulo modulo) //siempre los post son iguales, solo cambia el nombre de la entidad
         {
 
-            _context.Add(profesor);
+            _context.Add(modulo);
             await _context.SaveChangesAsync();
-            return Ok(profesor);
+            return Ok(modulo);
         }
     }
 }
---
+++ InnovateHubITM/InnovateHubITM.API/Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using InnovateHubITM.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InnovateHubITM.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        //Notación diamante es el nombre de la entidad <>, luego de esto se debe agregar la misma clase en plural
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Estudiante> Estudiantes { get; set; }
+        public DbSet<Inscripcion> Inscripciones { get; set; }
+        public DbSet<Comentario> Comentarios { get; set; }
+        public DbSet<Modulo> Modulos { get; set; }
+        public DbSet<Profesor> Profesores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+
+        }
+    }
+}
