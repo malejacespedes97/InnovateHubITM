f6e332a895dd16a8903c09489f33e74b8729c3d5
María Alejandra Correa <malejacespedes@gmail.com>
2023-10-12 01:54:57 -0500

    Entidades y DataContext

--- InnovateHubITM/InnovateHubITM.Shared/Entities/Estudiante.cs
+++ InnovateHubITM/InnovateHubITM.Shared/Entities/Estudiante.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,24 @@ namespace InnovateHubITM.Shared.Entities
 {
     public class Estudiante
     {
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre del estudiante")]
+        [MaxLength(50)]
+        [Required(ErrorMessage = "El nombre del estudiante es obligatorio")]
+        public string Nombre { get; set; } = null!;
+
+        [Display(Name = "Apellido del estudiante")]
+        [MaxLength(50)]
+        [Required(ErrorMessage = "El Apellido del estudiante es obligatorio")]
+        public string Apellidos { get; set; } = null!;
+
+        [Display(Name = "Correo del estudiante")]
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [DataType(DataType.EmailAddress)]
+        public string Correo{ get; set; } = null!;
+
+        public ICollection<Inscripcion> Inscripciones { get; set; } = null!;
+
     }
 }
